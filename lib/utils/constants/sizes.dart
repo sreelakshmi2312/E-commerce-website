6599db38a9a3fc789c55b0ca77c9e086e4bca9c1
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-05 15:27:32 +0900

    Addded:Size constants for app

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,78 @@
+class TAppsizes{
+  TAppsizes._();
+
+  //padding and margin sizes
+
+  static const double xs=4.0;
+  static const double sm=8.0;
+  static const double md=16.0;
+  static const double lg=24.0;
+  static const double xl=32.0;
+
+ //iconsizes
+
+ static const double iconxs=12;
+ static const double iconsm=16;
+ static const double iconmd=24;
+ static const double iconlg=32;
+
+ //buttonsizes
+
+ static const double buttonheight=18;
+ static const double buttonradius=12;
+ static const double buttonwidth=120;
+
+ //appbar size
+
+ static const double appbarheight=56.0;
+
+ //image size
+ static const double imagethumbsize=80.0;
+
+ //default spacing
+
+ static const double defaultspacing=24;
+ static const double spacebtwitems=16;
+ static const double spacebtwsections=32;
+
+ //border radius
+ static const double bordersmall=4;
+ static const double bordermedium=8;
+ static const double borderlarge=12;
+ 
+
+ //divider height
+
+ static const double dividerheight=1.0;
+
+ //product sizes
+
+ static const double productimagesize=160;
+ static const double productimageradius=16;
+ static const double productitemsize=120;
+
+//input field sizes
+
+static const double inputfieldradius=12;
+static const double inputfieldspacing=16;
+
+//card sizes
+
+static const double cardsizelarge=16;
+static const double cardsizemedium=12;
+static const double cardsizesmall=10;
+static const double cardsizexs=6;
+static const double cardelevation=2;
+
+//image carousel height
+
+static const double imagecarouselheight=200;
+
+//loading indicator size
+
+static const double loadingindicator=36;
+
+//grid view spacing 
+
+static const double gridviewspacing=16;
+}
