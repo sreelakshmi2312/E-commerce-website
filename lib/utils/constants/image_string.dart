43e11ec603e15e5834a80bbd123221892589ae59
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-06 17:38:17 +0900

    Added:Image constants

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,7 @@
+class TImageString{
+  
+  //onboarding images
+  static const String onboardingimage1='assets/images/onboarding_images.dart/sammy-line-sailor-on-mast-looking-through-telescope.gif';
+  static const String onboardingimage2='assets/images/onboarding_images.dart/sammy-line-woman-in-shopping-cart-placing-order-on-smartphone.gif';
+  static const String onboardingimage3='assets/images/onboarding_images.dart/sammy-line-man-and-dog-delivering-packages-on-a-moped.gif';
+}
