7f39ce80c5da61f81f6f482b304a828852e6386f
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-06 17:39:01 +0900

    Created:helper functions

---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class THelperfunctions{
+
+ static Size screenSize(){
+  return MediaQuery.of(Get.context!).size;
+ }
+ static double screenWidth(){
+   return MediaQuery.of(Get.context!).size.width;
+ }
+ static double screenHeight(){
+   return MediaQuery.of(Get.context!).size.height;
+ }
+}
