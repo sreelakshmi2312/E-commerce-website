88de79b4db8ef84a787e6472d221cb03e6678505
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-06 17:37:35 +0900

    Added:onboarding screen controller

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class OnBoargingController extends GetxController{
+   static OnBoargingController get instance => Get.find();
+
+   //update selectedpageindex 
+
+   void updateSelectedPage(int index){
+
+   }
+   //Move to next page on scrolling 
+
+   void moveToNextPage(){}
+
+   // jumo to a page on selection of dot
+
+   void moveToSelectedPage(int index){}
+
+   //skip to last page 
+
+   void skipToFinalPage(){}
+   
+   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../utils/theme/theme.dart';
+import 'features/authentication/screens/onboarding.dart';
+import 'package:get/get.dart';
 
 void main() {
 
@@ -12,27 +14,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'E-commerce Application',
        themeMode: ThemeMode.system,
        theme:TAppTheme.lightTheme,
        darkTheme: TAppTheme.darkTheme,
-       home: const MyHomePage(),
+       home: const OnBoardingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body:Center(child: Text('Ecommerce App'),),
-      );
-  }
-}
