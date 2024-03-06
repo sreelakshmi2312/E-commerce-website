60b2a533a692f8225c32e9ee86d9e965a5de94a4
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-06 17:38:31 +0900

    Added:String constants

---
+++ lib/utils/constants/text_string.dart
@@ -0,0 +1,18 @@
+class TApptexts{
+  
+  //onboarding Titles
+  static const String onboardingtext1="Choose your product";
+  static const String onboardingtext2="Select Payment Method";
+  static const String onboardingtext3="Deliver at your doorstep";
+  
+  //onboarding subtitles
+  static const String onboardingsubtext1="Welcome to the world of unlimited choices - your perfect product awaits!";
+  static const String onboardingsubtext2="For seamless transaction choose your payment method";
+  static const String onboardingsubtext3="From our doorstep to yours.";
+
+  //home titles
+
+  static const String hometitle="Good day for shopping";
+  static const String homesubtitle="Jo Bob";
+
+}
