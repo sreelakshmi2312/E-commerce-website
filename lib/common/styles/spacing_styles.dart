1ff6f8c4c83d2c26fae1ae4643b167f7cd2b2141
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-08 11:58:07 +0900

    Added: spacing styles

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,14 @@
+
+import 'package:ecommerceapp/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class TAppstyles{
+  TAppstyles._();
+  static const EdgeInsetsGeometry paddingstyle=EdgeInsets.only(
+    left:TAppsizes.defaultspacing,
+    top:TAppsizes.appbarheight,
+    bottom:TAppsizes.defaultspacing,
+    right:TAppsizes.defaultspacing,
+
+  );
+}
