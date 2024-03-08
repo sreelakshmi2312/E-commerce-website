0870d9e79dc86a4c0755b150004324951aa3c1af
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-03-08 11:59:26 +0900

    Added: constant image strings

--- lib/utils/constants/image_string.dart
+++ lib/utils/constants/image_string.dart
@@ -4,4 +4,26 @@ class TImageString{
   static const String onboardingimage1='assets/images/onboarding_images.dart/sammy-line-sailor-on-mast-looking-through-telescope.gif';
   static const String onboardingimage2='assets/images/onboarding_images.dart/sammy-line-woman-in-shopping-cart-placing-order-on-smartphone.gif';
   static const String onboardingimage3='assets/images/onboarding_images.dart/sammy-line-man-and-dog-delivering-packages-on-a-moped.gif';
+
+  //logos
+
+  static const String darkapplogo='assets/logos/t-store-splash-logo-white.png';
+  static const String lightapplogo='assets/logos/t-store-splash-logo-black.png';
+
+  //brand icons
+
+  static const String acericon='assets/icons/brands/acer_logo.png';
+  static const String adidasicon='assets/icons/brands/adidas-logo.png';
+  static const String appleicon='assets/icons/brands/apple-logo.png';
+  static const String hermanmillericon='assets/icons/brands/herman-miller-logo.png';
+  static const String ikeaicon='assets/icons/brands/ikea_logo.png';
+  static const String jordanicon='assets/icons/brands/jordan-logo.png';
+  static const String kenwoodicon='assets/icons/brands/kenwood-logo.png';
+  static const String nikeicon='assets/icons/brands/nike.png';
+  static const String pumaicon='assets/icons/brands/puma-logo.png';
+  static const String zaraicon='assets/icons/brands/zara-logo.png';
+
+
+
+
 }
